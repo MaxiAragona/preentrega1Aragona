4b08cf85fe96ecd6c734784c8d06754d43bef3d0
maxiaragona <aragona_9@hotmail.com>
2023-09-28 11:32:28 -0300

    arreglando el programa con un while

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
 function calcularResultadoMateria() {
     let materia = prompt("Bienvenid@s al calculador de promedios :D  Por favor ingrese el nombre de la materia a consultar:");
     
-    let nota1 = parseFloat(prompt("Ingrese la nota del primer cuatrimestre:"));
-    if (isNaN(nota1)) {
-      alert("La nota del primer cuatrimestre no es un número válido.");
-      return;
+    let nota1;
+    while (true) {
+        nota1 = parseFloat(prompt("Ingrese la nota del primer cuatrimestre:"));
+        if (!isNaN(nota1)) {
+            break; // Salir del bucle si la nota es válida
+        }
+        alert("La nota del primer cuatrimestre no es un número válido.");
     }
     
-    let nota2 = parseFloat(prompt("Ingrese la nota del segundo cuatrimestre:"));
-    if (isNaN(nota2)) {
-      alert("La nota del segundo cuatrimestre no es un número válido.");
-      return;
+    let nota2;
+    while (true) {
+        nota2 = parseFloat(prompt("Ingrese la nota del segundo cuatrimestre:"));
+        if (!isNaN(nota2)) {
+            break; // Salir del bucle si la nota es válida
+        }
+        alert("La nota del segundo cuatrimestre no es un número válido.");
     }
     
-    let nota3 = parseFloat(prompt("Ingrese la nota del tercer cuatrimestre:"));
-    if (isNaN(nota3)) {
-      alert("La nota del tercer cuatrimestre no es un número válido.");
-      return;
+    let nota3;
+    while (true) {
+        nota3 = parseFloat(prompt("Ingrese la nota del tercer cuatrimestre:"));
+        if (!isNaN(nota3)) {
+            break; // Salir del bucle si la nota es válida
+        }
+        alert("La nota del tercer cuatrimestre no es un número válido.");
     }
   
     let sumaNotas = nota1 + nota2 + nota3;
@@ -24,18 +33,18 @@ function calcularResultadoMateria() {
   
     let resultado;
     switch (true) {
-      case promedio >= 7:
-        resultado = "Felicitaciones :) aprobaste la materia !";
-        break;
-      case promedio >= 4:
-        resultado = "Debes rendir en diciembre";
-        break;
-      default:
-        resultado = "Debes rendir en marzo";
-        break;
+        case promedio >= 7:
+            resultado = "Felicitaciones :) aprobaste la materia !";
+            break;
+        case promedio >= 4:
+            resultado = "Debes rendir en diciembre";
+            break;
+        default:
+            resultado = "Debes rendir en marzo";
+            break;
     }
   
     alert("Para la Materia: " + materia + "\nTu Promedio es: " + promedio.toFixed(2) + "\nPor lo tanto : " + resultado);
-  }
-  
-  calcularResultadoMateria();
+}
+
+calcularResultadoMateria();
