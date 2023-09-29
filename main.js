8b03307bc716839f247e296ddf16139da4b90db4
maxiaragona <aragona_9@hotmail.com>
2023-09-28 23:41:13 -0300

    arreglo de nota

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
 function calcularResultadoMateria() {
     let materia = prompt("Bienvenid@s al calculador de promedios :D  Por favor ingrese el nombre de la materia a consultar:");
-    
+
     let nota1;
     while (true) {
         nota1 = parseFloat(prompt("Ingrese la nota del primer cuatrimestre:"));
-        if (!isNaN(nota1)) {
-            break; 
+        if (!isNaN(nota1) && nota1 >= 0 && nota1 <= 10) {
+            break;
         }
-        alert("La nota del primer cuatrimestre no es un número válido.");
+        alert("La nota del primer cuatrimestre debe ser un número válido entre 0 y 10.");
     }
-    
+
     let nota2;
     while (true) {
         nota2 = parseFloat(prompt("Ingrese la nota del segundo cuatrimestre:"));
-        if (!isNaN(nota2)) {
-            break; 
+        if (!isNaN(nota2) && nota2 >= 0 && nota2 <= 10) {
+            break;
         }
-        alert("La nota del segundo cuatrimestre no es un número válido.");
+        alert("La nota del segundo cuatrimestre debe ser un número válido entre 0 y 10.");
     }
-    
+
     let nota3;
     while (true) {
         nota3 = parseFloat(prompt("Ingrese la nota del tercer cuatrimestre:"));
-        if (!isNaN(nota3)) {
-            break; 
+        if (!isNaN(nota3) && nota3 >= 0 && nota3 <= 10) {
+            break;
         }
-        alert("La nota del tercer cuatrimestre no es un número válido.");
+        alert("La nota del tercer cuatrimestre debe ser un número válido entre 0 y 10.");
     }
-  
+
     let sumaNotas = nota1 + nota2 + nota3;
     let promedio = sumaNotas / 3;
-  
+
     let resultado;
     switch (true) {
         case promedio >= 7:
@@ -43,7 +43,7 @@ function calcularResultadoMateria() {
             resultado = "Debes rendir en marzo";
             break;
     }
-  
+
     alert("Para la Materia: " + materia + "\nTu Promedio es: " + promedio.toFixed(2) + "\nPor lo tanto : " + resultado);
 }
 
