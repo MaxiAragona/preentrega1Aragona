b3e09e5661524b734d0ea71af0b94c37ce8065b1
maxiaragona <aragona_9@hotmail.com>
2023-09-28 23:54:26 -0300

    listo para entregar

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 function calcularResultadoMateria() {
-    let materia = prompt("Bienvenid@s al calculador de promedios :D  Por favor ingrese el nombre de la materia a consultar:");
+    let materia = prompt("BIENVENID@S AL CALCULADOR DE PROMEDIOS :  \nPor favor ingrese el nombre de la materia a consultar:");
 
     let nota1;
     while (true) {
         nota1 = parseFloat(prompt("Ingrese la nota del primer cuatrimestre:"));
-        if (!isNaN(nota1) && nota1 >= 0 && nota1 <= 10) {
+        if (!isNaN(nota1) && nota1 >= 1 && nota1 <= 10) {
             break;
         }
-        alert("La nota del primer cuatrimestre debe ser un número válido entre 0 y 10.");
+        alert("La nota del primer cuatrimestre debe ser un número válido entre 1 y 10.");
     }
 
     let nota2;
     while (true) {
         nota2 = parseFloat(prompt("Ingrese la nota del segundo cuatrimestre:"));
-        if (!isNaN(nota2) && nota2 >= 0 && nota2 <= 10) {
+        if (!isNaN(nota2) && nota2 >= 1 && nota2 <= 10) {
             break;
         }
-        alert("La nota del segundo cuatrimestre debe ser un número válido entre 0 y 10.");
+        alert("La nota del segundo cuatrimestre debe ser un número válido entre 1 y 10.");
     }
 
     let nota3;
     while (true) {
         nota3 = parseFloat(prompt("Ingrese la nota del tercer cuatrimestre:"));
-        if (!isNaN(nota3) && nota3 >= 0 && nota3 <= 10) {
+        if (!isNaN(nota3) && nota3 >= 1 && nota3 <= 10) {
             break;
         }
-        alert("La nota del tercer cuatrimestre debe ser un número válido entre 0 y 10.");
+        alert("La nota del tercer cuatrimestre debe ser un número válido entre 1 y 10.");
     }
 
     let sumaNotas = nota1 + nota2 + nota3;
@@ -44,7 +44,7 @@ function calcularResultadoMateria() {
             break;
     }
 
-    alert("Para la Materia: " + materia + "\nTu Promedio es: " + promedio.toFixed(2) + "\nPor lo tanto : " + resultado);
+    alert(`PARA LA MATERIA: ${materia} \nTU PROMEDIO ES: ${promedio.toFixed(2)} \nPOR LO TANTO: ${resultado}`);
 }
 
 calcularResultadoMateria();
